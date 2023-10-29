e2dafb59408aa23f320857205bf37aae8d1176b1
esfredson <federicofrasquet@gmail.com>
2023-10-29 11:04:12 +0100

    injecting user data in sidebar and navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -93,6 +93,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -106,6 +107,8 @@ const Sidebar = ({
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
+
+
   return (
     <Box component="nav">
       {isSidebarOpen && (
@@ -161,36 +164,68 @@ const Sidebar = ({
                       }}
                       sx={{
                         backgroundColor:
-                         active === lcText
+                          active === lcText
                             ? theme.palette.secondary[300]
                             : "transparent",
                         color:
-                        active === lcText
+                          active === lcText
                             ? theme.palette.primary[600]
-                            : theme.palette.secondary[100]
-                    }}
-
+                            : theme.palette.secondary[100],
+                      }}
                     >
                       <ListItemIcon
                         sx={{
-                            ml: "2rem",
-                            color:
-                             active === lcText
-                                ? theme.palette.primary[600]
-                                : theme.palette.secondary[200]
+                          ml: "2rem",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
                         }}
                       >
                         {icon}
                       </ListItemIcon>
                       <ListItemText primary={text} />
                       {active === lcText && (
-                        <ChevronRightOutlined sx={{ ml: "auto "}} />
+                        <ChevronRightOutlined sx={{ ml: "auto " }} />
                       )}
                     </ListItemButton>
                   </ListItem>
-                )
+                );
               })}
             </List>
+          </Box>
+
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+                <Box textAlign="left">
+                  <Typography
+                    fontWeight="bold"
+                    fontSize="0.9rem"
+                    sx={{ color: theme.palette.secondary[100] }}
+                  >
+                    {user.name}
+                  </Typography>
+                  <Typography
+                    fontSize="0.8rem"
+                    sx={{ color: theme.palette.secondary[200] }}
+                  >
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <SettingsOutlined
+                  sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+                />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
